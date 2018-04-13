33919bd58436f0acd7684fcf13fc6e49b635d4a3
Emanuel Quimper <quimperemanuel@gmail.com>
2018-04-13 00:47:52 -0400

    Create ProjectCard

---
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,38 @@
+import { Pane, Text } from 'evergreen-ui';
+import * as React from 'react';
+import { pure, withHandlers } from 'recompose';
+import styled from 'styled-components';
+
+interface P {
+  name: string;
+  id: string;
+  onCardPress: (id: string) => void;
+}
+
+interface InjectedProps {
+  onClick: () => void;
+}
+
+const enhance = withHandlers<P, InjectedProps>({
+  onClick: (props: P) => () => props.onCardPress(props.id),
+});
+
+const Card = styled(Pane).attrs({
+  appearance: 'tint3',
+  elevation: 1,
+  hoverElevation: 3,
+})`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  border-radius: 5px;
+`;
+
+const ProjectCard = enhance(({ name, onClick }: P & InjectedProps) => (
+  <Card onClick={onClick}>
+    <Text>{name}</Text>
+  </Card>
+));
+
+export default pure(ProjectCard);
--- src/containers/projects/Projects.tsx
+++ src/containers/projects/Projects.tsx
@@ -1,8 +1,10 @@
-import { Button, Pane, Text } from 'evergreen-ui';
+import { Button, Text } from 'evergreen-ui';
 import { observer } from 'mobx-react';
 import * as React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 import styled from 'styled-components';
 
+import { ProjectCard } from '@freelance-tool/components';
 import { store } from '@freelance-tool/models';
 import { ModalTypeEnum } from '@freelance-tool/types';
 
@@ -26,20 +28,12 @@ const Grid = styled.div`
   grid-auto-rows: minmax(200px, auto);
 `;
 
-const Card = styled(Pane).attrs({
-  appearance: 'tint3',
-  elevation: 1,
-  hoverElevation: 3,
-})`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  border-radius: 5px;
-`;
+interface P {}
+
+interface S {}
 
 @observer
-class Projects extends React.Component {
+class Projects extends React.Component<RouteComponentProps<P>, S> {
   state = {};
   _openNewProjectModal = () => {
     store.modalsManager.open({
@@ -49,21 +43,28 @@ class Projects extends React.Component {
     });
   }
 
+  _onCardPress = (id: string) => {
+    this.props.history.push(`/projects/${id}`);
+  }
+
   render() {
     const { projects } = store;
     return (
       <Root>
         <TopWrapper>
-          <Text size={900}>My Project</Text>
+          <Text size={900}>My Projects</Text>
           <Button height={38} onClick={this._openNewProjectModal}>
             Create New Project
           </Button>
         </TopWrapper>
         <Grid>
           {projects.projects.map(el => (
-            <Card key={String(el.id)}>
-              <p>{el.name}</p>
-            </Card>
+            <ProjectCard
+              onCardPress={this._onCardPress}
+              {...el}
+              key={String(el.id)}
+              id={String(el.id)}
+            />
           ))}
         </Grid>
       </Root>
